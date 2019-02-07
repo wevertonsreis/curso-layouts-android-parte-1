47520a0797f3a54015da9b2cbd552de4d40bac4b
Weverton Reis <weverton.sant.reis@gmail.com>
2019-02-06 23:00:33 -0200

    1 - Introdução ao Constraint Layout

---
+++ AluraViagens/app/src/main/java/br/com/alura/aluraviagens/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.alura.aluraviagens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
